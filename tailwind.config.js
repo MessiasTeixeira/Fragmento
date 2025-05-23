bc54a1cae14c1b7da93c3cb82fb361c813774395
Messias Teixeira <messias4910@gmail.com>
2025-05-23 17:06:10 -0300

    commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['"Playfair Display"', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
